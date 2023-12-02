757ab1f592394e8fb238dfc06e6a04f64f2fe668
rumaisachowdhury <rumaisa.chowdhury@mail.utoronto.ca>
2023-12-01 23:39:23 -0500

    fixed imports

--- frontend/pet-pals/src/App.js
+++ frontend/pet-pals/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PetDetails from "./pages/PetDetails";
 import Applications from "./pages/Applications";
@@ -8,12 +8,11 @@ import ShelterDetails from "./pages/ShelterDetails";
 import NotFound from "./pages/404";
 import Layout from "./components/Header";
 import Notifications from "./pages/Notifications";
-import NotFound from "./pages/404/Index";
 import ShelterLogin from "./pages/ShelterLogin";
 import SeekerLogin from "./pages/SeekerLogin";
 import ShelterSignUp from "./pages/ShelterSignUp";
 import SeekerSignUp from "./pages/SeekerSignUp";
-import { LoginContext, useLoginContext } from './contexts/LoginContext';
+import { LoginContext, useLoginContext } from "./contexts/LoginContext";
 import ShelterProfile from "./pages/ShelterProfile";
 
 function App() {
@@ -33,7 +32,10 @@ function App() {
             <Route path="/shelter/signup/" exact element={<ShelterSignUp />} />
             <Route path="/seeker/signup/" exact element={<SeekerSignUp />} />
             <Route path="shelter/:id/" element={<ShelterDetails />} />
-            <Route path="profile/shelter/:shelter_id/" element={<ShelterProfile />} />
+            <Route
+              path="profile/shelter/:shelter_id/"
+              element={<ShelterProfile />}
+            />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
